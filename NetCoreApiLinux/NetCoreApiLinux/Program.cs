175a390e6afbacf7a9102ec08dc8806a3d72806f
alexander glukhov <al.glukhov.itmo@gmail.com>
2020-09-30 18:23:38 +0300

    + seq package and config

--- NetCoreApiLinux/NetCoreApiLinux/Program.cs
+++ NetCoreApiLinux/NetCoreApiLinux/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Formatting.Compact;
@@ -10,7 +11,12 @@ namespace NetCoreApiLinux
     {
         public static void Main(string[] args)
         {
+            var loggerConfiguration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
             Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(loggerConfiguration)
                 .Enrich.FromLogContext()
                 .WriteTo.Console(new RenderedCompactJsonFormatter())
                 .WriteTo.Debug(outputTemplate: DateTime.Now.ToString())
