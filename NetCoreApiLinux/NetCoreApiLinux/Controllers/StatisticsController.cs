3c0495f6c4801de177e96fdf89c7da12dbb85b35
alexander glukhov <al.glukhov.itmo@gmail.com>
2020-10-07 18:33:49 +0300

    events api

--- NetCoreApiLinux/NetCoreApiLinux/Controllers/StatisticsController.cs
+++ NetCoreApiLinux/NetCoreApiLinux/Controllers/StatisticsController.cs
@@ -15,15 +15,17 @@ namespace NetCoreApiLinux.Controllers
     public class StatisticsController : ControllerBase
     {
         private static readonly ILogger log = Log.ForContext<StatisticsController>();
-        private readonly IRepository<AppInfoDbo> appInfoRepository;
+        private readonly IAppInfoRepository appInfoRepository;
+        private readonly IStatisticsEventsHistoryRepository eventsRepository;
 
-        public StatisticsController(IRepository<AppInfoDbo> appInfoRepository)
+        public StatisticsController(IAppInfoRepository appInfoRepository, IStatisticsEventsHistoryRepository eventsRepository)
         {
             this.appInfoRepository = appInfoRepository;
+            this.eventsRepository = eventsRepository;
         }
 
         /// <summary>
-        /// Create or update statistics
+        /// Create or update statistics meta and merge events history
         /// </summary>
         /// <response code="200">AppInfo created</response>
         [HttpPost("appInfo")]
@@ -32,13 +34,22 @@ namespace NetCoreApiLinux.Controllers
             if (request.AppInfo.Id == null)
                 throw new ArgumentException("Id should not be null.");
 
-            appInfoRepository.AddOrUpdate(request.AppInfo.Adapt<AppInfoDbo>());
+            if (request.Events.Any(x => x.Description.Length > 50))
+                throw new ArgumentException("Description length should be <= 50.");
+
+            var events = request.Adapt<StatisticsEventsHistoryDbo>();
+            events.Id = request.AppInfo.Id.ToString();
+            var latestDate = eventsRepository.AddOrUpdate(events);
+
+            var newAppInfo = request.AppInfo.Adapt<AppInfoDbo>();
+            newAppInfo.LastUpdatedAt = latestDate ?? DateTimeOffset.Now;
+            appInfoRepository.AddOrUpdate(newAppInfo);
 
             log.Information("Called {Method}, {@Request}", nameof(AddOrUpdateAppInfo), request);
         }
 
         /// <summary>
-        /// Get statistics for all devices
+        /// Get statistics meta for all devices
         /// </summary>
         /// <response code="200">Array with AppInfos for all devices</response>
         [HttpGet("appInfo/all")]
@@ -47,6 +58,30 @@ namespace NetCoreApiLinux.Controllers
         {
             log.Information($"Called {nameof(GetAllAppInfos)}");
             return appInfoRepository.GetAll().Select(x => x.Adapt<AppInfo>()).ToArray();
+        }
+
+        /// <summary>
+        /// Get statistics meta for device by id
+        /// </summary>
+        /// <response code="200">AppInfo for device with provided id</response>
+        [HttpGet("appInfo/{id}")]
+        [ProducesResponseType(typeof(AppInfo), 200)]
+        public AppInfo GetAppInfoById(Guid id)
+        {
+            log.Information($"Called {nameof(GetAppInfoById)}");
+            return appInfoRepository.Find(id.ToString()).Adapt<AppInfo>();
+        }
+
+        /// <summary>
+        /// Get statistics events history for device by id
+        /// </summary>
+        /// <response code="200">Array with StatisticsEvent for device with provided id</response>
+        [HttpGet("appInfo/{id}/events-history")]
+        [ProducesResponseType(typeof(StatisticsEvent[]), 200)]
+        public StatisticsEvent[] GetStatisticsEventsHistoryByAppInfoId(Guid id)
+        {
+            log.Information($"Called {nameof(GetStatisticsEventsHistoryByAppInfoId)}");
+            return eventsRepository.Find(id.ToString()).Events.Select(x => x.Adapt<StatisticsEvent>()).ToArray();
         }
     }
 }
