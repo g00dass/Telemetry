52d03fbda11046cd5868af2888e438207798a9f3
alexander glukhov <al.glukhov.itmo@gmail.com>
2020-10-15 16:07:53 +0300

    no exe on linux

--- NetCoreApiLinux/build_integration_tests.ps1
+++ NetCoreApiLinux/build_integration_tests.ps1
@@ -1,3 +1,3 @@
 docker-compose -f ./DataLayer.IntegrationTests/docker-compose.yml up --build -d
 dotnet build MongoMigrations/MongoMigrations.csproj
-./MongoMigrations/bin/Debug/netcoreapp3.1/MongoMigrations.exe
+./MongoMigrations/bin/Debug/netcoreapp3.1/MongoMigrations
