1f89dcae9da68f43b989339f463696843cdc744e
alexander glukhov <al.glukhov.itmo@gmail.com>
2020-10-15 20:27:33 +0300

    divide to two composes

--- NetCoreApiLinux/DataLayer.IntegrationTests/startup.ps1
+++ NetCoreApiLinux/DataLayer.IntegrationTests/startup.ps1
@@ -1,3 +1,2 @@
-Start-Sleep -s 10
 ./MongoMigrations/bin/Debug/netcoreapp3.1/MongoMigrations
 dotnet test DataLayer.IntegrationTests
--- NetCoreApiLinux/DataLayer.IntegrationTests/tests.ps1
+++ NetCoreApiLinux/DataLayer.IntegrationTests/tests.ps1
@@ -1,2 +1,4 @@
-docker-compose up --build -d
-docker-compose down
+docker-compose -f docker-compose-mongo.yml up --build -d
+docker-compose -f docker-compose-tests.yml up --build
+docker-compose -f docker-compose-mongo.yml down
+docker-compose -f docker-compose-tests.yml down
