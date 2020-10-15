96d89bc7a3340da36ddf665aacf89367db178f43
alexander glukhov <al.glukhov.itmo@gmail.com>
2020-10-15 19:52:45 +0300

    exec not working, try run tests on start

---
+++ NetCoreApiLinux/DataLayer.IntegrationTests/startup.ps1
@@ -0,0 +1,3 @@
+Start-Sleep -s 10
+./MongoMigrations/bin/Debug/netcoreapp3.1/MongoMigrations
+dotnet test DataLayer.IntegrationTests
--- NetCoreApiLinux/DataLayer.IntegrationTests/tests.ps1
+++ NetCoreApiLinux/DataLayer.IntegrationTests/tests.ps1
@@ -1,4 +1,2 @@
 docker-compose up --build -d
-docker-compose exec tests ./MongoMigrations/bin/Debug/netcoreapp3.1/MongoMigrations
-docker-compose exec tests dotnet test DataLayer.IntegrationTests
 docker-compose down
